a5754f2421778e045d1403595728d21be37cd2ed
Paulo Deen <paulo.deen@gmail.com>
2024-01-28 14:35:15 +0100

    Move build query string from seeker live to page content

--- lib/bentoo/page_content.ex
+++ lib/bentoo/page_content.ex
@@ -27,25 +27,27 @@ defmodule Bentoo.PageContent do
     |> Enum.uniq()
   end
 
-  def build_query_string(%{
-        block_elements: block_elements,
-        single_elements: single_elements
-      }) do
-    html_tags =
-      case MapSet.size(block_elements) == 0 do
-        true ->
-          single_elements
-
-        false ->
-          Enum.reduce(block_elements, block_elements, fn block_element, acc ->
-            Enum.reduce(single_elements, acc, fn single_element, acc ->
-              MapSet.put(acc, "#{block_element} #{single_element}")
-            end)
-          end)
-      end
-
+  def build_query_string(html_tags) do
     html_tags
+    |> expand_html_tags()
     |> MapSet.to_list()
     |> Enum.join(", ")
+  end
+
+  defp expand_html_tags(%{
+         block_elements: block_elements,
+         single_elements: single_elements
+       }) do
+    case MapSet.size(block_elements) == 0 do
+      true ->
+        single_elements
+
+      false ->
+        Enum.reduce(block_elements, block_elements, fn block_element, acc ->
+          Enum.reduce(single_elements, acc, fn single_element, acc ->
+            MapSet.put(acc, "#{block_element} #{single_element}")
+          end)
+        end)
+    end
   end
 end
--- lib/bentoo_web/live/seeker_live.ex
+++ lib/bentoo_web/live/seeker_live.ex
@@ -93,25 +93,5 @@ defmodule BentooWeb.SeekerLive do
     assign(socket, :form, to_form(changeset))
   end
 
-  def build_query_string(%{
-        block_elements: block_elements,
-        single_elements: single_elements
-      }) do
-    html_tags =
-      case MapSet.size(block_elements) == 0 do
-        true ->
-          single_elements
-
-        false ->
-          Enum.reduce(block_elements, block_elements, fn block_element, acc ->
-            Enum.reduce(single_elements, acc, fn single_element, acc ->
-              MapSet.put(acc, "#{block_element} #{single_element}")
-            end)
-          end)
-      end
-
-    html_tags
-    |> MapSet.to_list()
-    |> Enum.join(", ")
-  end
+  defdelegate build_query_string(html_tags), to: PageContent
 end
